1ac14405824f6eb6a5348f10a82617affbbe309b
Pteropus12 <streamdroid12@gmail.com>
2023-11-01 06:56:06 +0530

    First Commit

--- Loops_FlowControl/z_exercises/src/Solutions.java
+++ Loops_FlowControl/z_exercises/src/Solutions.java
@@ -1,8 +1,11 @@
 public class Solutions {
+
     public static void main(String args[]){
+        int x =0;
         for (int i = 0 ; i <= 5; i++){
             System.out.println("i = "+ i);
+            x++;
         }
-        System.out.println("i after the loop is :" + i);
+        System.out.println("i after the loop is :" + x);
     }
 }
